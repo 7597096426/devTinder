e64df62ab91250063cf336c2d6fc64c715064ebe
tejpaly <tejpaly@abpnetwork.com>
2025-05-09 15:01:22 +0530

    mongoose connection

--- src/app.js
+++ src/app.js
@@ -1,30 +1,16 @@
 const express = require('express');
 const app = express();
-const { adminMiddlware } = require('./middleware/admin');
+const connectDb = require('./config/database');
 
-// Middleware for admin routes
-app.use('/admin', adminMiddlware);
+connectDb().then(()=>{
+    console.log('db connected successfully');
+    app.listen(7777, () => {
+        console.log('server running successfully');
+    });
+}).catch((err)=>{
+    console.log('error in db connection');
+}
+);
 
-// Admin routes
-app.get('/admin/getAllUser', (req, res) => {
-    res.send('all users data');
-});
 
-app.get('/admin/getProductList', (req, res) => {
-    res.send('all product list data');
-});
 
-// Error handler
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something went wrong');
-});
-
-// 404 handler
-app.use((req, res) => {
-    res.status(404).send('Page not found'); 
-});
-
-app.listen(7777, () => {
-    console.log('server running successfully');
-});
